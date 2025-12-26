a74da71b51f584fedaf5d3d349b62cd4bc2126ca
Muhammad Shoaib <muhammad.shoaib@psw.gov.pk>
2025-12-26 12:20:42 +0500

    Set explicit publicPath for production to fix chunk loading

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = (env, argv) => {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].bundle.js",
     chunkFilename: "[name].chunk.js",
-    publicPath: "auto",
+    publicPath: isProduction ? "https://post.shoaibarif.site/" : "auto",
     clean: true
   },
   resolve: {
